67d3b618771803f93052efdfde8b0eab367df488
Cappuccino <cappuccino0093@outlook.com>
2022-08-09 11:27:13 -0600

    Class 9

---
+++ School/School/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace School;
+
+class School
+{
+	internal string? name;
+	internal string? address;
+	internal int? fundationAge;
+	internal string? ceo = "Freddy Vega";
+
+	internal static void Ring() => Console.Beep();
+}
+
+class Program
+{
+	static void Main()
+	{
+		School school = new();
+		school.name = "Platzi Academy";
+		school.address = "Internet";
+		school.fundationAge = 2002;
+		School.Ring();
+	}
+}
